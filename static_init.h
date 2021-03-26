8232ee17f5ac148b43df8d202d5191929023af99
liushuai <liushuai02@bilibili.com>
2021-03-26 21:23:33 +0800

    feat: static_init

---
+++ static_init.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <functional>
+
+class StaticCall {
+public:
+    explicit StaticCall(const std::function<void()>& func) noexcept {
+        func();
+    }
+};
+
+#define __STATIC_CALL_CAT_(a, b)    a##b
+#define __STATIC_CALL_CAT(a, b)     __STATIC_CALL_CAT_(a, b)
+#define static_init(func)           namespace { \
+                                    StaticCall __STATIC_CALL_CAT(__static_call__var, __COUNTER__)(func);  /* NOLINT(cert-err58-cpp) */ \
+                                    }
---
+++ tests/static_init.cpp
@@ -0,0 +1,14 @@
+#include "static_init.h"
+
+static_init([]{
+    printf("hello, ");
+})
+
+static_init([]{
+    printf("world");
+})
+
+int main() {
+//    static_init([]{}) // compile error
+    return 0;
+}
