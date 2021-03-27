82c991b7342128ccab113cb097dd1dd429221615
liushuai <liushuai02@bilibili.com>
2021-03-27 17:00:58 +0800

    feat: refine test

--- copy_on_write.h
+++ copy_on_write.h
@@ -17,15 +17,18 @@ class copy_on_write {
     ST _data = std::make_shared<T>();
     std::mutex _mutex;
 public:
-    inline void readOp(const std::function<void(ST)>& op) {
-        auto tmp = std::atomic_load(&_data); // ensure use_count++ and atomic
-        op(std::move(tmp));
-    }
     void writeOp(const std::function<void(const ST&)>& op) {
         ST d = std::make_shared<T>();
         std::lock_guard<std::mutex> lock(_mutex);
         *d = *_data; // deep copy
         op(d);
         std::atomic_store(&_data, d);
+    }
+    inline void readOp(const std::function<void(ST)>& op) {
+        auto tmp = std::atomic_load(&_data); // ensure use_count++ and atomic
+        op(std::move(tmp));
+    }
+    inline ST operator->() {
+        return std::atomic_load(&_data);
     }
 };
--- tests/copy_on_write.cpp
+++ tests/copy_on_write.cpp
@@ -1,15 +1,63 @@
 #include <cstdio>
 #include <vector>
+#include <thread>
 #include "copy_on_write.h"
 
 int main() {
     copy_on_write<std::vector<int>> l;
-    l.writeOp([](const std::shared_ptr<std::vector<int>>& l) {
-        l->push_back(1);
-        printf("%d, use_count:%ld\n", (*l)[0], l.use_count());
+    using SC = std::shared_ptr<std::vector<int>>;
+
+    const size_t WriteNum = 10000;
+    volatile size_t deleteNum = 0;
+    volatile size_t remainNum = 0;
+    volatile size_t readTimes = 0;
+    std::atomic<bool> stop{false};
+
+    auto startTime = std::chrono::steady_clock::now();
+    std::thread writeThread([&]{
+        for(;;) {
+            static size_t i;
+            if(i++ == WriteNum) return;
+            l.writeOp([](const SC& l) {
+                l->push_back(i);
+                printf("+: %zu, uc:%ld\n", l->size(), l.use_count());
+            });
+        }
     });
-    l.readOp([](const std::shared_ptr<std::vector<int>>& l) {
-        printf("%d, use_count:%ld\n", (*l)[0], l.use_count());
+    std::thread deleteThread([&]{
+        for(;;) {
+            if (stop) return;
+            l.writeOp([&](const SC& l) {
+                if (l->empty()) return;
+                l->erase(l->cbegin());
+                deleteNum++;
+                printf("-: %zu, uc:%ld\n", l->size(), l.use_count());
+            });
+        }
     });
-    return 0;
+    std::thread readThread([&]{
+        for(;;) {
+            l.readOp([&](const SC& l) {
+                readTimes++;
+                printf(" : %zu, uc:%ld\n", l->size(), l.use_count());
+            });
+            if (stop) return;
+        }
+    });
+
+    writeThread.join();
+    stop = true;
+    deleteThread.join();
+    readThread.join();
+    auto endTime = std::chrono::steady_clock::now();
+
+    remainNum = l->size();
+    auto timeUs = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count();
+    printf("summary: +:%zu, -:%zu, =:%zu, r:%zu, timeUs:%llu\n", WriteNum, deleteNum, remainNum, readTimes, timeUs);
+    if (WriteNum - deleteNum == remainNum) {
+        printf("success!\n");
+        return EXIT_SUCCESS;
+    } else {
+        return EXIT_FAILURE;
+    }
 }
