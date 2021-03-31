4335af7d566c93d2d6d976fcd839c1d7cb794a8a
liushuai <liushuai02@bilibili.com>
2021-03-31 11:13:22 +0800

    feat: refine thread_safe test

--- tests/thread_safe.cpp
+++ tests/thread_safe.cpp
@@ -5,9 +5,18 @@
 
 int main() {
     const size_t kMaxCount = 10000;
-    thread_safe<std::vector<int>, Type::ReadWrite> container;
-    container.read()->size();
-    container.write()->push_back(1);
+
+    thread_safe<std::vector<int>> container;
+    // or
+    // thread_safe<std::vector<int>, Type::Default> container;
+    container->size();
+    container->push_back({});
+    container->pop_back();
+
+    thread_safe<std::vector<int>, Type::ReadWrite> container_rw;
+    container_rw.read()->size();
+    container_rw.write()->push_back({});
+    container_rw.write()->pop_back();
 
     std::vector<std::future<void>> results;
     std::atomic<size_t> count{0};
@@ -17,9 +26,18 @@ int main() {
         auto t = std::async(std::launch::async, [&]{
             for(;;) {
                 if (count++ >= kMaxCount) return;
-                container.write()->push_back(count);
+
+                /// test container
+                container->push_back({});
                 // or
-                container.lock(LockType::Write, [&](std::vector<int>* c){
+                container.lock([&](std::vector<int>* c){
+                    c->push_back(count);
+                });
+
+                /// test container_rw
+                container_rw.write()->push_back(count);
+                // or
+                container_rw.lock(LockType::Write, [&](std::vector<int>* c){
                     c->push_back(count);
                 });
             }
@@ -30,7 +48,8 @@ int main() {
     for(auto& t : results){
         t.get();
     }
-    printf("%zu\n", container->size());
+    printf("container:%s\n", container->size() == 2*kMaxCount ? "success" : "failed");
+    printf("container_rw:%s\n", container_rw->size() == 2*kMaxCount ? "success" : "failed");
 
     return 0;
 }
