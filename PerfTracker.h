a3804f61333a52887f10631626dfc0007a7da9cb
liushuai <770722922@qq.com>
2022-06-10 10:33:03 +0800

    feat: refine PerfTracker

--- PerfTracker.h
+++ PerfTracker.h
@@ -4,12 +4,16 @@
 #include <cstdint>
 #include <vector>
 #include <string>
+#include <sstream>
+#include <iostream>
+#include <functional>
 
 /**
  * 用于精细的代码耗时分析
  * 为了方便调用 推荐临时定义如下宏
  * #define TRACK(msg)  perfTracker.track(std::string("L:") + std::to_string(__LINE__) + "\t"#msg)
  */
+template<typename T = std::chrono::milliseconds>
 class PerfTracker {
     using TimePoint = std::chrono::time_point<std::chrono::steady_clock>;
 
@@ -21,13 +25,18 @@ public:
 
     PerfTracker(const PerfTracker &) = delete;
     void operator=(const PerfTracker &) = delete;
-    PerfTracker(PerfTracker &&) = default;
+    PerfTracker(PerfTracker &&) noexcept = default;
 
     ~PerfTracker() {
-      dump();
+        auto result = dump();
+        if (dumpHandle) {
+            dumpHandle(std::move(result));
+        }
+        else {
+            std::cout << result << std::flush;
+        }
     }
 
-    template<typename T>
     inline uint64_t duration(TimePoint end, TimePoint start) {
         return std::chrono::duration_cast<T>(end - start).count();
     }
@@ -36,21 +45,25 @@ public:
         points_.emplace_back(std::move(tag), std::chrono::steady_clock::now());
     }
 
-    template<typename T = std::chrono::milliseconds>
-    void dump() {
+private:
+    std::string dump() {
         TimePoint end = std::chrono::steady_clock::now();
-        printf("PerfTracker Report begin:\n");
-        printf("total duration: %llu\n", duration<T>(end, start_));
+        std::ostringstream stream;
+        stream << "PerfTracker Report begin:\n";
+        stream << "total duration: " << duration(end, start_) << '\n';
         TimePoint lastTime = start_;
         for (auto &item: points_) {
-            printf("-> from last: %llu, from start: %llu\ttag: %s\n",
-                   duration<T>(item.second, lastTime),
-                   duration<T>(item.second, start_),
-                   item.first.c_str());
+            stream << "-> from last: " << duration(item.second, lastTime)
+                   << ", from start: " << duration(item.second, start_)
+                   << "\ttag: " << item.first << '\n';
             lastTime = item.second;
         }
-        printf("PerfTracker Report end\n");
+        stream << "PerfTracker Report end\n";
+        return stream.str();
     }
+
+public:
+    std::function<void(std::string result)> dumpHandle;
 
 private:
     std::vector<std::pair<std::string, TimePoint>> points_;
--- tests/PerfTracker.cpp
+++ tests/PerfTracker.cpp
@@ -5,7 +5,11 @@
 #define TRACK(msg)  perfTracker.track(std::string("L:") + std::to_string(__LINE__) + "\t"#msg)
 
 int main() {
-    PerfTracker perfTracker;
+    PerfTracker<> perfTracker;
+    // dumpHandle is optional
+    perfTracker.dumpHandle = [](const std::string& result){
+        std::cout << "PerfTracker result:\n" << result;
+    };
     std::this_thread::sleep_for(std::chrono::milliseconds(1));
     TRACK("after 1");
     std::this_thread::sleep_for(std::chrono::milliseconds(2));
