b21fe6fadb77a21111ccd40457f4c21c66ae55f0
liushuai <770722922@qq.com>
2022-12-27 15:48:18 +0800

    feat: Reference add reset

--- Reference.hpp
+++ Reference.hpp
@@ -5,40 +5,43 @@
 #include "noncopyable.hpp"
 
 template <typename T>
-class Reference : noncopyable
-{
-public:
-    void retain()
-    {
-        count_++;
-        if (count_ == 1) {
-            t_ = new T;
-        }
+class Reference : noncopyable {
+ public:
+  void retain() {
+    count_++;
+    if (count_ == 1) {
+      t_ = new T;
     }
+  }
 
-    void release()
-    {
-        if (count_ < 1) {
-            return;
-        }
-        count_--;
-        if (count_ == 0) {
-            delete t_;
-            t_ = nullptr;
-        }
+  void release() {
+    if (count_ < 1) {
+      return;
     }
-
-    inline T *get()
-    {
-        return t_;
+    count_--;
+    if (count_ == 0) {
+      delete t_;
+      t_ = nullptr;
     }
+  }
 
-    inline T *operator->()
-    {
-        return t_;
+  void reset() {
+    if (count_ > 0) {
+      count_ = 0;
+      delete t_;
+      t_ = nullptr;
     }
+  }
 
-private:
-    T *t_ = nullptr;
-    int count_ = 0;
+  inline T *get() {
+    return t_;
+  }
+
+  inline T *operator->() {
+    return t_;
+  }
+
+ private:
+  T *t_ = nullptr;
+  int count_ = 0;
 };
